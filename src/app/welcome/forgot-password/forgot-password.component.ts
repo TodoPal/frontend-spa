fffeccdb37bc379c0703c8bfaa208ec17338bc0a
HbotondS <hbotond97@gmail.com>
2024-05-31 11:31:46 +0300

    added work in progress for forgot password page

--- src/app/welcome/forgot-password/forgot-password.component.ts
+++ src/app/welcome/forgot-password/forgot-password.component.ts
@@ -3,7 +3,16 @@ import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-forgot-password',
-  templateUrl: './forgot-password.component.html',
+  // templateUrl: './forgot-password.component.html',
+  template: `
+    <div class="hero min-h-screen bg-base-200">
+      <div class="hero-content text-center">
+        <div class="max-w-md">
+          <h1 class="text-5xl font-bold">Work in progress...</h1>
+        </div>
+      </div>
+    </div>
+  `,
   styleUrls: [ './forgot-password.component.scss' ]
 })
 export class ForgotPasswordComponent {
